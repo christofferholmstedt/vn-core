4c1d00a0f485c423626b53a19d0b2fa650a9e279
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2014-02-24 14:45:27 +0100

    Updated bash provisioning script.

--- scripts/install-gcc4.9.0.sh
+++ scripts/install-gcc4.9.0.sh
@@ -111,74 +111,15 @@ done
 chown -R root:root /opt/tmp/src_unpacked
 
 ####################################
-# OLD #1 - Download required sources
+# Apply patches
 ####################################
-# mkdir -pv /opt/arm/src/orig
-# cd /opt/arm/src/orig
-#
-# # Some pre-downloading is required due to AdaCores CDN structure with files
-# # only available at different hashes.
-# # Go to http://libre.adacore.com/download/ and download the following three
-# # packages in advance, before starting vagrant.
-#
-# declare -a ADACORE_FILES=("gcc-4.5-gpl-2012-src.tgz"
-#                           "gdb-7.4-gpl-2012-src.tgz"
-#                           "gnat-gpl-2012-src.tgz");
-#
-# declare -a ALL_FILES=("${ADACORE_FILES[@]}");
-#
-# for ADACORE_FILE in "${ADACORE_FILES[@]}"
-# do
-#     # Check if file has been pre-downloaded
-#     if [ ! -f /home/vagrant/host/downloads/$ADACORE_FILE ];
-#     then
-#         echo "$ADACORE_FILE not found!"
-#         exit 1 # Abort if file is missing.
-#
-#     # Check if file has been pre-download and file has been copied to
-#     # /opt/arm/src/orig/
-#     elif [ -f /home/vagrant/host/downloads/$ADACORE_FILE ] && [ ! -f /opt/arm/src/orig/$ADACORE_FILE ];
-#     then
-#         echo "$ADACORE_FILE found, copying to /opt/arm/src/orig/"
-#         cp -v /home/vagrant/host/downloads/$ADACORE_FILE /opt/arm/src/orig/
-#     else
-#         echo "$ADACORE_FILE has already been copied to /opt/arm/src/orig/ ,skipping..."
-#     fi
-# done
-#####################################
-# OLD #2 - Download required sources
-#####################################
-# mkdir -pv /opt/arm/src/orig
-# cd /opt/arm/src/orig
-#
-# declare -a GNU_FILES=("http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.gz"
-#                       "http://ftp.gnu.org/gnu/gmp/gmp-5.1.3.tar.gz"
-#                       "http://ftp.gnu.org/gnu/mpc/mpc-1.0.2.tar.gz"
-#                       "http://ftp.gnu.org/gnu/mpfr/mpfr-3.1.2.tar.gz");
-#
-# for GNU_URL in "${GNU_FILES[@]}"
-# do
-#     GNU_FILE=$(basename $GNU_URL)
-#     ALL_FILES+=("$GNU_FILE")
-#     if [ ! -f /opt/arm/src/orig/$GNU_FILE ]; then
-#         echo "$GNU_FILE not found, downloading..."
-#         wget -P /opt/arm/src/orig/ $GNU_URL
-#     else
-#         echo "$GNU_FILE found, skipping downloading."
-#     fi
-# done
-#####################################
-# OLD - Unpack all files
-#####################################
-# mkdir -pv /opt/arm/src/src
-# cd /opt/arm/src/src/
-#
-# for DOWNLOADED_FILE in "${ALL_FILES[@]}"
-# do
-#     if [ -f "/opt/arm/src/orig/$DOWNLOADED_FILE" ];
-#     then
-#         tar -xvf /opt/arm/src/orig/$DOWNLOADED_FILE
-#     fi
-# done
-#
-# chown -R root:root /opt/arm/src/src/
+if [ ! -f /opt/tmp/gcc-patches-applied ]
+then
+    echo "GCC patches not applied yet, applying patches..."
+    cd /opt/tmp/src_unpacked/gcc
+    patch -p1 < ../../0001-Set-the-target-for-a-bare-metal-environment.patch
+    patch -p1 < ../../0002-Added-enable-cross-gnattools-flag-for-bare-metal-env.patch
+    touch "/opt/tmp/gcc-patches-applied"
+else
+    echo "GCC patches already applied, skipping."
+fi
