b720173a04b020c8c4f23ece58500bbb81267719
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2014-02-24 13:56:59 +0100

    Fixes to the gcc installation script.

--- scripts/install-gcc4.9.0-gdb7.7.sh
+++ scripts/install-gcc4.9.0-gdb7.7.sh
@@ -34,25 +34,75 @@ mkdir -pv /opt/gcc-4.9-arm
 # Is it perhaps possible to download these files from the GNU GCC repository?
 # All files from one archive would make it a little bit easier to write this
 # script.
-wget -P /opt/tmp/ https://gmplib.org/download/gmp/gmp-4.3.2.tar.bz2
-wget -P /opt/tmp/ http://www.mpfr.org/mpfr-3.1.2/mpfr-3.1.2.tar.bz2
-wget -P /opt/tmp/ ftp://ftp.gnu.org/gnu/mpc/mpc-1.0.2.tar.gz
-wget -P /opt/tmp/ http://isl.gforge.inria.fr/isl-0.11.1.tar.bz2
-wget -P /opt/tmp/ http://www.bastoul.net/cloog/pages/download/count.php3?url=./cloog-0.18.1.tar.gz
-wget -P /opt/tmp/ http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.bz2
-wget -P /opt/tmp/ http://gcc.1065356.n5.nabble.com/attachment/1011154/1/0002-Added-enable-cross-gnattools-flag-for-bare-metal-env.patch
-wget -P /opt/tmp/ http://gcc.1065356.n5.nabble.com/attachment/1011154/0/0001-Set-the-target-for-a-bare-metal-environment.patch
-wget -P /opt/tmp/ ftp://ftp.funet.fi/pub/mirrors/sources.redhat.com/pub/gdb/releases/gdb-7.7.tar.bz2
-wget -P /opt/tmp/ ftp://sourceware.org/pub/newlib/newlib-2.0.0.tar.gz
+declare -a DOWNLOADS=("https://gmplib.org/download/gmp/gmp-4.3.2.tar.bz2"
+                      "http://www.mpfr.org/mpfr-3.1.2/mpfr-3.1.2.tar.bz2"
+                      "ftp://ftp.gnu.org/gnu/mpc/mpc-1.0.2.tar.gz"
+                      "http://www.bastoul.net/cloog/pages/download/cloog-0.18.1.tar.gz"
+                      "http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.bz2"
+                      "ftp://ftp.funet.fi/pub/mirrors/sources.redhat.com/pub/gdb/releases/gdb-7.7.tar.bz2"
+                      "ftp://sourceware.org/pub/newlib/newlib-2.0.0.tar.gz"
+                      "http://isl.gforge.inria.fr/isl-0.11.1.tar.bz2");
+
+declare -a PATCHES=("http://gcc.1065356.n5.nabble.com/attachment/1011154/1/0002-Added-enable-cross-gnattools-flag-for-bare-metal-env.patch"
+                    "http://gcc.1065356.n5.nabble.com/attachment/1011154/0/0001-Set-the-target-for-a-bare-metal-environment.patch");
+
+### Download software
+for DOWNLOAD_URL in "${DOWNLOADS[@]}"
+do
+    FILE=$(basename $DOWNLOAD_URL)
+    if [ ! -f /opt/tmp/$FILE ]; then
+        echo "$FILE not found, downloading..."
+        wget -nc -P /opt/tmp/ $DOWNLOAD_URL
+    else
+        echo "$FILE found, skipping downloading."
+    fi
+done
+
+### Download GCC
+# Using Github mirror instead of the "official" mirror because of better
+# download speeds.
+# Official: git://gcc.gnu.org/git/gcc.git (http://gcc.gnu.org/wiki/GitMirror)
+# Inofficial: https://github.com/mirrors/gcc
+if [ ! -d /opt/tmp/src_unpacked/gcc ];
+then
+    echo "GCC git clone was not found, cloning to /opt/tmp/src_unpacked/gcc"
+    git clone https://github.com/mirrors/gcc /opt/tmp/src_unpacked/gcc
+else
+    echo "GCC git clone already available in /opt/tmp/src_unpacked/gcc, skipping."
+fi
+
+### Download patches
+for PATCH_URL in "${PATCHES[@]}"
+do
+    PATCH=$(basename $PATCH_URL)
+    echo /opt/tmp/$PATCH
+    if [ ! -f /opt/tmp/$PATCH ]; then
+        echo "$PATCH not found, downloading..."
+        wget -nc -P /opt/tmp/ $PATCH_URL
+    else
+        echo "$PATCH found, skipping downloading."
+    fi
+done
 
 #####################################
 # Unpack all files
 #####################################
-if [ -f /opt/tmp/gmp-4.3.2.tar.bz2 ] && [ ! -f /opt/tmp/gmp-4.3.2 ];
-then
-    echo "gmp-4.3.2 found, unpacking to /opt/tmp/gmp-4.3.2/"
-    tar -xvf /opt/tmp/gmp-4.3.2.tar.bz2 -C /opt/tmp/src_unpacked
-fi
+for DOWNLOAD_URL in "${DOWNLOADS[@]}"
+do
+    FILE=$(basename $DOWNLOAD_URL)
+    # Remove all extensions from the filenames, only two different types in the
+    # array DOWNLOADS.
+    DIR=$(basename $FILE .tar.gz)
+    DIR=$(basename $DIR .tar.bz2)
+
+    if [ -f /opt/tmp/$FILE ] && [ ! -d /opt/tmp/src_unpacked/$DIR ];
+    then
+        echo "$FILE found, unpacking to /opt/tmp/src_unpacked/$DIR"
+        tar -xvkf /opt/tmp/$FILE  -C /opt/tmp/src_unpacked
+    else
+        echo "$FILE already available in /opt/tmp/src_unpacked/$DIR"
+    fi
+done
 
 ### Final touch to set proper ownership to all files.
 chown -R root:root /opt/tmp/src_unpacked
