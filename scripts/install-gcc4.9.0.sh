a672b56d3ad4c60549ec07f2fcc3e47760c25b78
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2014-02-26 13:55:31 +0100

    Updated the installationscript.
    Still not compiling properly "gcc -lisl" doesn't work. Don't know where to
    continue my bug hunt.

--- scripts/install-gcc4.9.0.sh
+++ scripts/install-gcc4.9.0.sh
@@ -7,7 +7,7 @@ source /home/vagrant/host/scripts/native-gcc4.9.0-build-variables.sh
 source /home/vagrant/host/scripts/arm-gcc4.9.0-build-variables.sh
 source /home/vagrant/host/scripts/libdir-variables.sh
 
-echo "$(date) NATIVE_GCC_TARGET=$NATIVE_GCC_TARGET" > $LOGFILE
+echo "$(date) NATIVE_GCC_TARGET=$NATIVE_GCC_TARGET" >> $LOGFILE
 echo "$(date) NATIVE_GCC_DIR=$NATIVE_GCC_DIR" >> $LOGFILE
 echo "$(date) NATIVE_GCC_BIN_PATH=$NATIVE_GCC_BIN_PATH" >> $LOGFILE
 echo "$(date) ARM_GCC_TARGET=$ARM_GCC_TARGET" >> $LOGFILE
@@ -27,7 +27,8 @@ CORES=5
 # GIT VERSION (only takes effect during initial provisioning),
 # that is the first time you run vagrant up
 GIT_REVISION="2a9110cb500e6754c28864a6dfaed185b367516c"
-
+#GCC_SNAPSHOT_URL="ftp://ftp.mpi-sb.mpg.de/pub/gnu/mirror/gcc.gnu.org/pub/gcc/snapshots/4.9-20140223/gcc-4.9-20140223.tar.bz2"
+GCC_SNAPSHOT_URL="ftp://ftp.gwdg.de/pub/misc/gcc/snapshots/4.9-20140223/gcc-4.9-20140223.tar.bz2"
 #############################################
 # Installation of GNAT
 #############################################
@@ -41,9 +42,9 @@ sudo apt-get -y install automake autoconf gcc-4.5-base \
     texinfo libncurses5-dev libmpfr-dev libgmp3-dev libmpc-dev libzip-dev \
     python-dev libexpat1-dev libelf1 libelfg0 elfutils libppl9 libcloog-ppl0
 
-sudo apt-get -y install ia32-libs lib32gcc1 libc6-i386 lib32z1 lib32stdc++6 \
-    lib32asound2 lib32ncurses5 lib32gomp1 lib32z1-dev lib32bz2-dev \
-    g++-multilib gcc-multilib
+# sudo apt-get -y install ia32-libs lib32gcc1 libc6-i386 lib32z1 lib32stdc++6 \
+#     lib32asound2 lib32ncurses5 lib32gomp1 lib32z1-dev lib32bz2-dev \
+#     g++-multilib gcc-multilib
 
 echo "$(date) Finished installing required packages with apt-get." >> $LOGFILE
 
@@ -102,18 +103,35 @@ echo "$(date) Finished downloading required source packages." >> $LOGFILE
 # download speeds.
 # Official: git://gcc.gnu.org/git/gcc.git (http://gcc.gnu.org/wiki/GitMirror)
 # Inofficial: https://github.com/mirrors/gcc
+# if [ ! -d /opt/tmp/src_unpacked/gcc ];
+# then
+#     echo "$(date) Cloning GCC git repository from github mirror (this will take some time, 1.11GB to download)." >> $LOGFILE
+#     echo "Cloning GCC git repository from github mirror (this will take some time, 1.11GB to download), about 20 minutes in total with 100Mbit/s download speed limited by Github upload speed."
+#     git clone https://github.com/mirrors/gcc /opt/tmp/src_unpacked/gcc
+#     cd /opt/tmp/src_unpacked/gcc
+#     echo "$(date) Changing to GIT revision $GIT_REVISION." >> $LOGFILE
+#     git checkout $GIT_REVISION
+#     echo "$(date) Finished cloning GCC git repository." >> $LOGFILE
+# else
+#     echo "$(date) GCC git clone already available in /opt/tmp/src_unpacked/gcc, skipping." >> $LOGFILE
+# fi
+
 if [ ! -d /opt/tmp/src_unpacked/gcc ];
 then
-    echo "$(date) Cloning GCC git repository from github mirror (this will take some time, 1.11GB to download)." >> $LOGFILE
-    git clone https://github.com/mirrors/gcc /opt/tmp/src_unpacked/gcc
-    cd /opt/tmp/src_unpacked/gcc
-    echo "$(date) Changing to GIT revision $GIT_REVISION." >> $LOGFILE
-    git checkout $GIT_REVISION
-    echo "$(date) Finished cloning GCC git repository." >> $LOGFILE
-else
-    echo "$(date) GCC git clone already available in /opt/tmp/src_unpacked/gcc, skipping." >> $LOGFILE
-fi
+    FILE=$(basename $GCC_SNAPSHOT_URL)
+    DIR=$(basename $FILE .tar.bz2)
 
+    echo "$(date) Downloading GCC snapshot $FILE from $GCC_SNAPSHOT_URL" >> $LOGFILE
+    wget -nc -P /opt/tmp/ $GCC_SNAPSHOT_URL
+
+    echo "$(date) Uncompressing $FILE to /opt/tmp/src_unpacked/$DIR." >> $LOGFILE
+    tar -xvkf /opt/tmp/$FILE  -C /opt/tmp/src_unpacked
+
+    echo "$(date) Renaming $DIR to gcc" >> $LOGFILE
+    mv /opt/tmp/src_unpacked/$DIR /opt/tmp/src_unpacked/gcc
+else
+    echo "$(date) GCC already available in /opt/tmp/src_unpacked/gcc, skipping." >> $LOGFILE
+fi
 
 ### Download patches
 echo "$(date) Downloading required GCC patches." >> $LOGFILE
@@ -281,43 +299,44 @@ else
     echo "$(date) $ARCH $PROG already compiled, skipping." >> $LOGFILE
 fi
 
-### QUIRK steps, extra steps to fix something unknown.
-if [ ! -f /opt/tmp/did-reinstall-of-native-binutils ]
-then
-    echo "$(date) [Quirk] Started with quirk fix." >> $LOGFILE
-    # rm -rf /opt/gcc-4.9-native/*
-    cd /opt/tmp/b-native/b-gmp
-    echo "$(date) [Quirk] Installing b-gmp." >> $LOGFILE
-    make install
-    cd /opt/tmp/b-native/b-mpfr
-    echo "$(date) [Quirk] Installing b-mpfr." >> $LOGFILE
-    make install
-    cd /opt/tmp/b-native/b-mpc
-    echo "$(date) [Quirk] Installing b-mpc." >> $LOGFILE
-    make install
-    cd /opt/tmp/b-native/b-isl
-    echo "$(date) [Quirk] Installing b-isl." >> $LOGFILE
-    make install
-    cd /opt/tmp/b-native/b-cloog
-    echo "$(date) [Quirk] Installing b-cloog." >> $LOGFILE
-    make install
-
-    # Re-compile binutils
-    echo "$(date) [Quirk] Recompiling binutils." >> $LOGFILE
-    cd /opt/tmp/b-native/b-binutils
-    ../../src_unpacked/binutils-2.24/configure --prefix=$NATIVE_GCC_DIR \
-        --verbose --disable-nls --target=$NATIVE_GCC_TARGET \
-        --enable-interwork --enable-multilib --disable-werror \
-        --with-cloog=$NATIVE_GCC_DIR --with-isl=$NATIVE_GCC_DIR \
-        --with-stage1-ldflags="-Wl,-rpath,/opt/gcc-4.9-native/lib" \
-        --enable-cloog-backend=isl
-    make -j$CORES
-    make install
-    echo "$(date) [Quirk] Finished recompiling binutils." >> $LOGFILE
-    touch /opt/tmp/did-reinstall-of-native-binutils
-    echo "$(date) [Quirk] Finished with quirk fix." >> $LOGFILE
-fi
-
+# ### QUIRK steps, extra steps to fix something unknown.
+# if [ ! -f /opt/tmp/did-reinstall-of-native-binutils ]
+# then
+#     echo "$(date) [Quirk] Started with quirk fix." >> $LOGFILE
+#     echo "$(date) [Quirk] Removing everything in /opt/gcc-4.9-native/*" >> $LOGFILE
+#     # rm -rf /opt/gcc-4.9-native/*
+#     cd /opt/tmp/b-native/b-gmp
+#     echo "$(date) [Quirk] Installing b-gmp." >> $LOGFILE
+#     make install
+#     cd /opt/tmp/b-native/b-mpfr
+#     echo "$(date) [Quirk] Installing b-mpfr." >> $LOGFILE
+#     make install
+#     cd /opt/tmp/b-native/b-mpc
+#     echo "$(date) [Quirk] Installing b-mpc." >> $LOGFILE
+#     make install
+#     cd /opt/tmp/b-native/b-isl
+#     echo "$(date) [Quirk] Installing b-isl." >> $LOGFILE
+#     make install
+#     cd /opt/tmp/b-native/b-cloog
+#     echo "$(date) [Quirk] Installing b-cloog." >> $LOGFILE
+#     make install
+# 
+#     # Re-compile binutils
+#     echo "$(date) [Quirk] Recompiling binutils." >> $LOGFILE
+#     cd /opt/tmp/b-native/b-binutils
+#     ../../src_unpacked/binutils-2.24/configure --prefix=$NATIVE_GCC_DIR \
+#         --verbose --disable-nls --target=$NATIVE_GCC_TARGET \
+#         --enable-interwork --enable-multilib --disable-werror \
+#         --with-cloog=$NATIVE_GCC_DIR --with-isl=$NATIVE_GCC_DIR \
+#         --with-stage1-ldflags="-Wl,-rpath,/opt/gcc-4.9-native/lib" \
+#         --enable-cloog-backend=isl
+#     make -j$CORES
+#     make install
+#     echo "$(date) [Quirk] Finished recompiling binutils." >> $LOGFILE
+#     touch /opt/tmp/did-reinstall-of-native-binutils
+#     echo "$(date) [Quirk] Finished with quirk fix." >> $LOGFILE
+# fi
+# 
 ### GCC
 PROG=b-gcc
 if [ ! -d /opt/tmp/b-$ARCH/$PROG ]
@@ -332,6 +351,7 @@ then
         --prefix=$NATIVE_GCC_DIR --enable-shared --with-system-zlib \
         --enable-threads=posix \
         --disable-werror --enable-checking=release --build=$NATIVE_GCC_TARGET \
+        --disable-multilib \
         --host=x86_64-linux-gnu --target=x86_64-linux-gnu \
         --with-gmp=$NATIVE_GCC_DIR \
         --with-mpfr=$NATIVE_GCC_DIR \
--- scripts/remove-installed-gcc-and-dependencies.sh
+++ scripts/remove-installed-gcc-and-dependencies.sh
@@ -6,10 +6,15 @@
 rm -rf /opt/tmp/b-arm-cross/*
 rm -rf /opt/tmp/b-native/*
 
+rm -rf /opt/gcc-4.9-native/*
+rm -rf /opt/gcc-4.9-arm/*
+
 if [ -f /opt/tmp/did-reinstall-of-native-binutils ];
 then
     rm /opt/tmp/did-reinstall-of-native-binutils
 fi
 
-rm -rf /opt/gcc-4.9-native/*
-rm -rf /opt/gcc-4.9-arm/*
+if [ -f /opt/tmp/gcc-patches-applied ];
+then
+    rm /opt/tmp/gcc-patches-applied
+fi
